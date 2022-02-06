9e665f4f1fa5afaddcacbe2fedcce6541bbb36b2
Churchil Yash Rajpal <85837940+churchil15@users.noreply.github.com>
2022-02-06 23:26:53 +0530

    Update Diff.java

--- Diff.java
+++ Diff.java
@@ -1,5 +1,6 @@
 public class Diff {
     public static void main(String[] args){
-        System.out.prinln("This is the program for difference");        
+        System.out.println("This is the program for difference");    
+        System.out.println("Checking the commit");
     }
 }
